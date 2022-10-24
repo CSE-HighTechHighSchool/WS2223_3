730c8cde3841ef22576ef260d058c4acd4a29e69
mbattista0 <58273489+mbattista0@users.noreply.github.com>
2022-10-24 14:12:56 -0400

    add field map clicking functionality

--- js/fieldDiagram.js
+++ js/fieldDiagram.js
@@ -1,3 +1,32 @@
-document.getElementsByClassName('field-point').forEach(element => {
+var fieldElementData;
+fetch('../data/fieldElementData.json')
+    .then(response => response.json())
+    .then(data => {fieldElementData = data;})
+    .then(promise => {
+        Array.prototype.forEach.call(document.getElementsByClassName('field-point'), element => {
+            element.onclick = changeFieldData;
+        });
+    });
+
+function updateDataElements(data, fieldElement) {
+    Object.entries(data).forEach(([key, val]) => {
+        document.getElementById(key).innerHTML = val;
+    })
+}
+
+function changeFieldData(event) {
+    let fieldElement = event.target.dataset.fieldElement;
     
-});
+    // remove message before first click and show data elements
+    document.getElementById("field-click-message").style.display = "none";
+    document.getElementById("field-data-elements").style.display = "block";
+
+    // change circle outline to indicate focus
+    Array.prototype.forEach.call(document.getElementsByClassName('field-point'), element => {
+        element.style.stroke = "black";
+    });
+    event.target.style.stroke = "blue";
+
+    // update with selected field element's data
+    updateDataElements(fieldElementData[fieldElement], fieldElement);
+}
