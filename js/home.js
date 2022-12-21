c87c5587e0ec9f2c02f55c2d9cd9bff4de19a911
vgabbita <gabbitavarun@gmail.com>
2022-12-21 09:14:01 -0500

    add js files

---
+++ js/home.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCB38tZI5slskcr5EHZhOBBTnlpc1JgQ9o",
+    authDomain: "vexwebsite-6a4ee.firebaseapp.com",
+    projectId: "vexwebsite-6a4ee",
+    storageBucket: "vexwebsite-6a4ee.appspot.com",
+    messagingSenderId: "846412076753",
+    appId: "1:846412076753:web:1d2fb8c4e5b264ced7d7f0"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
