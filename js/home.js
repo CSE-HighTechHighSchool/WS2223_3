1d458143a42bfadb6b25da86c02b19f56ac41a00
mbattista0 <58273489+mbattista0@users.noreply.github.com>
2023-01-08 20:07:08 -0500

    implement get dataset/chartjs

--- js/home.js
+++ js/home.js
@@ -1,26 +1,26 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-  import { getAuth, signOut} 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, signOut }
   from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
-  
-  import { getDatabase, ref, set, update, child, get, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
-  
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCB38tZI5slskcr5EHZhOBBTnlpc1JgQ9o",
-    authDomain: "vexwebsite-6a4ee.firebaseapp.com",
-    projectId: "vexwebsite-6a4ee",
-    storageBucket: "vexwebsite-6a4ee.appspot.com",
-    messagingSenderId: "846412076753",
-    appId: "1:846412076753:web:1d2fb8c4e5b264ced7d7f0"
-  };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+import { getDatabase, ref, set, update, child, get, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
-   //Initialize authentication
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCB38tZI5slskcr5EHZhOBBTnlpc1JgQ9o",
+  authDomain: "vexwebsite-6a4ee.firebaseapp.com",
+  projectId: "vexwebsite-6a4ee",
+  storageBucket: "vexwebsite-6a4ee.appspot.com",
+  messagingSenderId: "846412076753",
+  appId: "1:846412076753:web:1d2fb8c4e5b264ced7d7f0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//Initialize authentication
 const auth = getAuth();
 
 //Return instance of your app's FRD
@@ -29,7 +29,7 @@ const db = getDatabase(app);
 // ---------------------// Get reference values -----------------------------
 let userLink = document.getElementById('userLink');   // user name for number
 let signOutLink = document.getElementById('signOut'); // sign-out link
-let welcome = document .getElementById('welcome');    // welcome header
+let welcome = document.getElementById('welcome');    // welcome header
 //let resetLink = document.getElementById('reset');     // reset input link
 let currentUser = null;                               // initialize currentUser to null
 
@@ -38,7 +38,7 @@ let currentUser = null;                               // initialize currentUser 
 function getUserName() {
   // Grab the value for the 'keep logged in' switch
   let keepLoggedIn = localStorage.getItem('keepLoggedIn');
-  
+
   // Grab uder information passed in from signIn.js
   if (keepLoggedIn == 'yes') {
     currentUser = JSON.parse(localStorage.getItem('user'));
@@ -60,8 +60,8 @@ function signOutUser() {
     console.log(error);
   });
 }
-  
-  // Remove user info from local/session storage
+
+// Remove user info from local/session storage
 
 // Sign-out function that will remove user info from local/session storage and
 // sign-out from FRD
@@ -144,7 +144,7 @@ function unhHideBox() {
 
 // ----------------------Get a datum from FRD (single data point)---------------
 
- async function getData(userID, org, name){
+async function getData(userID, org, name) {
   const dbref = ref(db); // firebase paramter to get a reference to the database
 
   //Provide the path thrugh the nodes
@@ -153,131 +153,222 @@ function unhHideBox() {
       //To fet set of data, use snapshot.val()
       return snapshot.val();
     } else {
-      alert("This team has no information inputted. Please update information before loading.");
+      return null;
     }
   })
-  .catch((error) => {
-    alert("Unsuccessful, error" + error);
-  });
+    .catch((error) => {
+      alert("Unsuccessful, error" + error);
+    });
   return data;
- }
+}
 
 async function loadExistingData(userID, org, name) {
   let data = await getData(userID, org, name);
-  console.log(data);
-  document.getElementById('base').value = data['Drive Base'];
-  document.getElementById('rpm').value = data['RPM'];
-  document.getElementById('wheelSize').value = data['Wheel Size'];
-  document.getElementById('wheelType').value = data['Wheel Type'];
-  document.getElementById('shooter').value = data['Shooter'];
-  document.getElementById('roller').value = data['Roller'];
-  document.getElementById('expansion').value = data['Expansion'];
-  document.getElementById('auton').value = data['Auton'];
-  document.getElementById('awp').value = data['AWP'];
-  document.getElementById('ccwm').value = data['CCWM'];
-  document.getElementById('notes').value = data['Notes'];
+  if (data) {
+    document.getElementById('base').value = data['Drive Base'];
+    document.getElementById('rpm').value = data['RPM'];
+    document.getElementById('wheelSize').value = data['Wheel Size'];
+    document.getElementById('wheelType').value = data['Wheel Type'];
+    document.getElementById('shooter').value = data['Shooter'];
+    document.getElementById('roller').value = data['Roller'];
+    document.getElementById('expansion').value = data['Expansion'];
+    document.getElementById('auton').value = data['Auton'];
+    document.getElementById('awp').value = data['AWP'];
+    document.getElementById('ccwm').value = data['CCWM'];
+    document.getElementById('notes').value = data['Notes'];
+  }
   unhHideBox();
 }
 
-function inputData(userID, org, name, base, rpm, wheelSize, wheelType, shooter, roller, expansion, auton, awp, ccwm, notes){
+function inputData(userID, org, name, base, rpm, wheelSize, wheelType, shooter, roller, expansion, auton, awp, ccwm, notes) {
   // Must use brackets around variable names to use it as a key
-  update(ref(db, '/Organization/' + org  + '/' + name), {
+  update(ref(db, '/Organization/' + org + '/' + name), {
     'Drive Base': base,
     'RPM': rpm,
     'Wheel Size': wheelSize,
     'Wheel Type': wheelType,
     'Shooter': shooter,
     'Roller': roller,
-    'Expansion': expansion, 
+    'Expansion': expansion,
     'Auton': auton,
     'AWP': awp,
     'CCWM': ccwm,
     'Notes': notes
   })
-  .then(() => {
-    alert('Data inputted successfully.')
-  }).catch((error) => {
-    alert('There was an error. Error: ' + error)
+    .then(() => {
+      alert('Data inputted successfully.')
+    }).catch((error) => {
+      alert('There was an error. Error: ' + error)
+    });
+}
+
+function getScore(ccwm, expansion) {
+  let score = 1;
+  score *= ccwm;  // ccwm
+  score *= expansion;   // expansion
+  return score;
+}
+
+function createChart(teams, scores) {
+  const ctx = document.getElementById('scores-chart');
+  ctx.style.display = 'block';
+  const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: teams,
+      datasets: [
+        {
+          data: scores,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      responsive: true,                   // Re-size based on screen size
+      scales: {                           // x & y axes display options
+        y: {
+          beginAtZero: false,
+          title: {
+            display: true,
+            text: 'Score',
+            font: {
+              size: 20
+            },
+          }
+        }
+      },
+      plugins: {                          // title and legend display options
+        title: {
+          display: true,
+          text: 'Team Scores',
+          font: {
+            size: 24
+          },
+          padding: {
+            top: 10,
+            bottom: 30
+          }
+        },
+        legend: {
+          position: 'bottom'
+        }
+      }
+    }
   });
- }
+}
 
 // ---------------------------Get a month's data set --------------------------
 // Must be an async function because you need to get all the data from FRD
 // before you can process it for a table or graph
-async function getDataSet(userID, year, month){
-  let yearVal = document.getElementById('setYearVal');
-  let monthVal = document.getElementById('setMonthVal');
-
-  yearVal.textContent = `Year: ${year}`;
-  monthVal.textContent = `Month: ${month}`;
-  const days = [];
-  const temps = [];
+async function getDataSet(userID) {
+  const teams = [];
+  const bases = [];
+  const rpms = [];
+  const wheelSizes = [];
+  const wheelTypes = [];
+  const shooters = [];
+  const rollers = [];
+  const expansions = [];
+  const autons = [];
+  const awps = [];
+  const ccwms = [];
+  const notes = [];
+  const scores = [];
   const tbodyEl = document.getElementById('tbody-2'); // Select <tbody> from table
 
   const dbref = ref(db); // firbease paramter required for 'get'
 
   //Wait for all data to be pulled from the FRD
   //Provide the path thrugh the nodes to the data
-  await get(child(dbref, '/Organization/' + year + '/' + month)).then((snapshot) => {
+  await get(child(dbref, '/Organization/')).then((snapshot) => {
     if (snapshot.exists()) {
-      console.log(snapshot.val());
-
-      snapshot.forEach(child => {
-        console.log(child.key, child.val());
-        // Push values to the correct arrays
-        days.push(child.key);
-        temps.push(child.val());
+      Object.keys(snapshot.val()).forEach(org => {
+        Object.keys(snapshot.val()[org]).forEach(team => {
+          let data = snapshot.val()[org][team];
+          teams.push(org + team);
+          bases.push(data['Drive Base']);
+          rpms.push(data['RPM']);
+          wheelSizes.push(data['Wheel Size']);
+          wheelTypes.push(data['Wheel Type']);
+          shooters.push(data['Shooter']);
+          rollers.push(data['Roller']);
+          expansions.push(data['Expansion']);
+          autons.push(data['Auton']);
+          awps.push(data['AWP']);
+          ccwms.push(data['CCWM']);
+          notes.push(data['Notes']);
+          scores.push(getScore(data['CCWM'], data['Expansion']));
+        })
       });
     } else {
       alert("No data found");
-    }})
-  .catch((error) => {
-    alert("Unsuccessful, error" + error);
-  });
+    }
+  })
+    .catch((error) => {
+      alert("Unsuccessful, error" + error);
+    });
 
   // Dynamically add the table rows ro HTML
   tbodyEl.innerHTML = ''; // Clear the table
-  for(let i = 0; i < days.length; i++) {
-    addItemToTable(days[i], temps[i], tbodyEl);
+  for (let i = 0; i < teams.length; i++) {
+    addItemToTable(
+      [
+        teams[i],
+        bases[i],
+        rpms[i],
+        wheelSizes[i],
+        wheelTypes[i],
+        shooters[i],
+        rollers[i],
+        expansions[i],
+        autons[i],
+        awps[i],
+        ccwms[i],
+        scores[i],
+        notes[i]
+      ],
+      tbodyEl
+    );
   }
+
+  createChart(teams, scores);
 }
 
 // Add a item to the table of data
-function addItemToTable(day, temp, tbody) {
-  console.log(day, temp);
+function addItemToTable(teamData, tbody) {
   let tRow = document.createElement('tr');  // row
-  let td1 = document.createElement('td');  // column 1
-  let td2 = document.createElement('td'); // column 2
 
-  td1.innerHTML = day;
-  td2.innerHTML = temp;
-
-  tRow.appendChild(td1);
-  tRow.appendChild(td2);
+  teamData.forEach(datum => {
+    let td = document.createElement('td'); // column
+    td.innerHTML = datum;
+    tRow.appendChild(td);
+  });
 
   tbody.appendChild(tRow);
 }
 
 
 // -------------------------Delete a day's data from FRD ---------------------
-function deleteData(userID, year, month, day){
+function deleteData(userID, year, month, day) {
   remove(ref(db, '/Organization/' + year + '/' + month + '/' + day))
-  .then(() => {
-    alert('Data removed successfully.')
-  })
-  .catch((error) => {
-    alert('There was an error. Error: ' + error)
-  })
+    .then(() => {
+      alert('Data removed successfully.')
+    })
+    .catch((error) => {
+      alert('There was an error. Error: ' + error)
+    })
 }
 
 
 
 // --------------------------- Home Page Loading -----------------------------
-window.onload = function() {
+window.onload = function () {
   // ---------------------------------- Set Welcome Message -------------------------
   getUserName();
   hideBox();
-  if(currentUser == null) {
+  if (currentUser == null) {
     userLink.innerHTML = currentUser.name;
     userLink.classList.replace("nav-link", "btn");
     userLink.classList.add("btn-primary");
@@ -287,7 +378,7 @@ window.onload = function() {
     signOutLink.classList.replace("nav-link", "btn");
     signOutLink.classList.add("btn-success");
     signOutLink.href = "signIn.html";
-  
+
   }
   else {
     userLink.innerText = currentUser.firstName;
@@ -299,91 +390,89 @@ window.onload = function() {
     signOutLink.innerHTML = "Sign Out";
     signOutLink.classList.replace("btn", "nav-link");
     signOutLink.classList.add("btn-success");
-    document.getElementById('signOut').onclick = function(){
+    document.getElementById('signOut').onclick = function () {
       signOutUser();
     }
-  } 
+  }
 
   //Set Data
-//   document.getElementById('set').onclick = function() {
-//     const org = document.getElementById('org').value;
-//     const name = document.getElementById('name').value;
-//     const base = document.getElementById('base').value;
-//     const userID = currentUser.uid;
-//     setData(userID, org, name, base);
-// };
+  //   document.getElementById('set').onclick = function() {
+  //     const org = document.getElementById('org').value;
+  //     const name = document.getElementById('name').value;
+  //     const base = document.getElementById('base').value;
+  //     const userID = currentUser.uid;
+  //     setData(userID, org, name, base);
+  // };
 
-// Update data
-document.getElementById('update').onclick = function() {
-  const org = document.getElementById('org').value;
-  const name = document.getElementById('name').value;
-  unhHideBox();
-  const base = document.getElementById('base').value;
-  const rpm = document.getElementById('rpm').value;
-  const wheelSize = document.getElementById('wheelSize').value;
-  const wheelType = document.getElementById('wheelType').value;
-  const shooter = document.getElementById('shooter').value;
-  const roller = document.getElementById('roller').value;
-  const expansion = document.getElementById('expansion').value;
-  const auton = document.getElementById('auton').value;
-  const awp = document.getElementById('awp').value;
-  const ccwm = document.getElementById('ccwm').value;
-  const notes = document.getElementById('notes').value;
-  const userID = currentUser.uid;
-  inputData(userID, org, name, base, rpm, wheelSize, wheelType, shooter, roller, expansion, auton, awp, ccwm, notes);
-  
-
-}
-
-document.getElementById('reset').onclick = function() {
-  window.location.href = "home.html";
-}
-
-// get a datum
-document.getElementById('load-existing').onclick = function() {
-  const org = document.getElementById('org').value;
-  const name = document.getElementById('name').value;
-  const userID = currentUser.uid;
-  loadExistingData(userID, org, name);
-
-}
-// get a data set
-document.getElementById('getDataSet').onclick = function() {
-  const year = document.getElementById('getSetYear').value;
-  const month = document.getElementById('getSetMonth').value;
-  const userID = currentUser.uid;
-  getDataSet(userID, year, month);
-};
+  // Update data
+  document.getElementById('update').onclick = function () {
+    const org = document.getElementById('org').value;
+    const name = document.getElementById('name').value;
+    unhHideBox();
+    const base = document.getElementById('base').value;
+    const rpm = document.getElementById('rpm').value;
+    const wheelSize = document.getElementById('wheelSize').value;
+    const wheelType = document.getElementById('wheelType').value;
+    const shooter = document.getElementById('shooter').value;
+    const roller = document.getElementById('roller').value;
+    const expansion = document.getElementById('expansion').value;
+    const auton = document.getElementById('auton').value;
+    const awp = document.getElementById('awp').value;
+    const ccwm = document.getElementById('ccwm').value;
+    const notes = document.getElementById('notes').value;
+    const userID = currentUser.uid;
+    inputData(userID, org, name, base, rpm, wheelSize, wheelType, shooter, roller, expansion, auton, awp, ccwm, notes);
 
 
-// Delete a day's data
-document.getElementById('delete').onclick = function() {
-  const year = document.getElementById('delYear').value;
-  const month = document.getElementById('delMonth').value;
-  const day = document.getElementById('delDay').value;
-  const userID = currentUser.uid;
-  deleteData(userID, year, month, day);
+  }
 
-};
+  document.getElementById('reset').onclick = function () {
+    window.location.href = "home.html";
+  }
 
-// document.getElementById('reset').onclick = function() {
-//   resetLink.href = "index.html";
-// };
+  // get a datum
+  document.getElementById('load-existing').onclick = function () {
+    const org = document.getElementById('org').value;
+    const name = document.getElementById('name').value;
+    const userID = currentUser.uid;
+    loadExistingData(userID, org, name);
+
+  }
+  // get a data set
+  document.getElementById('get-dataset').onclick = function () {
+    const userID = currentUser.uid;
+    getDataSet(userID);
+  };
+
+
+  // Delete a day's data
+  document.getElementById('delete').onclick = function () {
+    const year = document.getElementById('delYear').value;
+    const month = document.getElementById('delMonth').value;
+    const day = document.getElementById('delDay').value;
+    const userID = currentUser.uid;
+    deleteData(userID, year, month, day);
+
+  };
+
+  // document.getElementById('reset').onclick = function() {
+  //   resetLink.href = "index.html";
+  // };
 }
   // ------------------------- Set Welcome Message -------------------------
 
-  
+
   // Get, Set, Update, Delete Sharkriver Temp. Data in FRD
   // Set (Insert) data function call
-  
+
 
   // Update data function call
-  
+
 
   // Get a datum function call
-  
+
 
   // Get a data set function call
-  
+
 
   // Delete a single day's data function call
